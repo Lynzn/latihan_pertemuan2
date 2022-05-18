7ae91a71e728d6ae7c32c505323fbdfe584c5edd
FlutLab.io <>
2022-05-18 05:57:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,27 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    title: "belajar Images",
+    home: BelajarGambarNetwork(),
+  ));
 }
 
-class MyHomePage extends StatelessWidget {
+class BelajarGambarNetwork extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Belajar Mobile Programming'),
+      appBar: AppBar(
+        title: Text("Belajar Image"),
+      ),
+      body: Center(
+        child: CachedNetworkImage(
+          imageUrl: "https://cdn.pixabay.com/photo/2019/11/10/17/36/indonesia-4616370_1280.jpg",
+          placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) => Text("Koneksi Error"),
         ),
-        // body: Center(
-        //     child: Text('Selamat Datang Di Pembelajaran Mobile Programming',
-        //         style: TextStyle(fontSize: 40, fontFamily: 'DancingScript')))
-        body: Container(
-          decoration: BoxDecoration(
-            color: const Color(0xff7c94b6),
-            image: const DecorationImage(
-              image: NetworkImage('https://awsimages.detik.net.id/community/media/visual/2022/03/25/manga-one-piece_43.webp?w=700&q=90%27'),
-              fit: BoxFit.cover,
-            ),
-            border: Border.all(
-              color: Colors.black,
-              width: 8,
-            ),
-            borderRadius: BorderRadius.circular(50),
-          ),
-          height: 280,
-          width: 280,
-          margin: EdgeInsets.all(50),
-        ));
+      ),
+    );
   }
 }
